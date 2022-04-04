f38be70ab3e9c855afc956619ea6cf69cdf2ff19
amal33q <53046858+amal33q@users.noreply.github.com>
2022-04-04 21:04:06 +0300

    Pages

---
+++ app/src/main/java/com/example/firbasedb/HealthNutrition.java
@@ -0,0 +1,45 @@
+package com.example.firbasedb;
+
+
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HealthNutrition extends AppCompatActivity {
+    Dialog mydialog;
+Button Add;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_health_nutrition);
+        mydialog = new Dialog(this);
+
+
+        // Write a message to the database
+
+    }
+
+    public void ShowPopup(View v){
+        TextView txtclose;
+       Button btn;
+        mydialog.setContentView(R.layout.popup);
+        txtclose=(TextView)  mydialog.findViewById(R.id.txtclose);
+        btn = (Button) mydialog.findViewById(R.id.GotoCart);
+        txtclose.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+               mydialog.dismiss();
+            }
+        });
+        mydialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+         mydialog.show();
+    }
+
+}
+
